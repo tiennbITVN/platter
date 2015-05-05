d0c3677beee295a4544edb189c83f90e29ae890f
Abraham Kuri <kurenn@icalialabs.com>
2015-05-05 15:08:02 -0500

    Adds api support gems with a class option

--- lib/platter/app_builder.rb
+++ lib/platter/app_builder.rb
@@ -10,5 +10,15 @@ module Platter
       template "Gemfile.erb", "Gemfile"
     end
 
+    def add_api_support
+      inject_into_file "Gemfile", after: "ruby \"#{Platter::RUBY_VERSION}\"" do
+        %Q{
+
+gem "versionist"
+gem "active_model_serializers", github: "rails-api/active_model_serializers", branch: "0-8-stable"
+        }
+      end
+    end
+
   end
 end
--- lib/platter/generators/app_generator.rb
+++ lib/platter/generators/app_generator.rb
@@ -9,6 +9,9 @@ module Platter
     class_option :skip_turbolinks, type: :boolean, default: true,
       desc: "Skips the turbolinks gem"
 
+    class_option :api, type: :boolean, default: false,
+      desc: "Adds API support gems"
+
     def finish_template
       invoke :platter
       super
@@ -16,12 +19,17 @@ module Platter
 
     def platter
       invoke :custom_gemfile
+      invoke :add_api_support
     end
 
     def custom_gemfile
       build :replace_gemfile
 
       bundle_command 'install'
+    end
+
+    def add_api_support
+      build :add_api_support if options[:api]
     end
 
     protected
