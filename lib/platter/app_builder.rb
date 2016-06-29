606ee4546c5748de63c6a1383ac3138d40ab66a2
Abraham Kuri <kurenn@icalialabs.com>
2016-06-29 16:23:02 -0500

    Removes the heroku subdomain from the exception notification mailer

--- lib/platter/app_builder.rb
+++ lib/platter/app_builder.rb
@@ -150,8 +150,8 @@ gem "active_model_serializers", github: "rails-api/active_model_serializers", br
   #Exception Notification configuration
   config.middleware.use ExceptionNotification::Rack,
   :email => {
-    :email_prefix => "[ProjectName] ",
-    :sender_address => %{"Staging Exception" <exception@#{app_name.downcase}-#{env}.herokuapp.com>},
+    :email_prefix => "[#{app_name}] ",
+    :sender_address => %{"Exception" <exception@#{app_name.downcase}-#{env}.com>},
     :exception_recipients => %w{}
   }
           },
